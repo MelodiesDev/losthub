7d98440542a0f96770833753028782f7ff794159
MelodiesDev <melodiesdev@proton.me>
2024-06-26 15:22:54 +1000

    feat: added scoreboard basics

--- src/main/kotlin/dev/melodies/utils/ScoreboardEnabler.kt
+++ src/main/kotlin/dev/melodies/utils/ScoreboardEnabler.kt
@@ -17,6 +17,9 @@ class ScoreboardEnabler : Listener {
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
 
+        val currentCoins = 100
+        val currentExp = 59
+
         // Create a Scoreboard Manager
         val manager: ScoreboardManager = Bukkit.getScoreboardManager()
 
@@ -30,30 +33,35 @@ class ScoreboardEnabler : Listener {
 
         val blank1: Score = objective.getScore("blank1")
         blank1.customName(Component.empty())
-        blank1.score = 6
+        blank1.score = 7
 
-        val score: Score = objective.getScore(player.name)
-        score.score = 5
+        val name: Score = objective.getScore(player.name)
+        name.customName(("<blue>|</blue> <gradient:aqua:light_purple:white:light_purple:aqua>${player.name}</gradient>").toMiniMessage())
+        name.score = 6
 
         val blank2: Score = objective.getScore("blank2")
         blank2.customName(Component.empty())
-        blank2.score = 4
+        blank2.score = 5
 
         val coins: Score = objective.getScore("coins")
-        coins.customName("<gold>Coins".toMiniMessage())
-        coins.score = 3
+        coins.customName(("<blue>|</blue> Coins: <gold>\$$currentCoins</gold>").toMiniMessage())
+        coins.score = 4
 
         val blank3: Score = objective.getScore("blank3")
         blank3.customName(Component.empty())
-        blank3.score = 2
+        blank3.score = 3
 
         val exp: Score = objective.getScore("exp")
-        exp.customName("<dark_purple>Experience".toMiniMessage())
-        exp.score = 1
+        exp.customName("<blue>|</blue> XP: <green>$currentExp%</green>".toMiniMessage())
+        exp.score = 2
 
         val blank4: Score = objective.getScore("blank4")
         blank4.customName(Component.empty())
-        blank4.score = 0
+        blank4.score = 1
+
+        val end: Score = objective.getScore("end")
+        end.customName("<dark_gray> << lostoasis.com >></dark_gray>".toMiniMessage())
+        end.score = 0
 
         // Assign the scoreboard to the player
         player.scoreboard = board
